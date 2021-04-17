41bbc6efbf7a6dc9ecc2358b6567e136dfaf6e73
sreeja002 <82311358+sreeja002@users.noreply.github.com>
2021-04-17 13:08:41 +0530

    create positive.py
    it will display positive values on screen

---
+++ positive.py
@@ -0,0 +1,11 @@
+list1 = [12,3,5,-5,-6,-1]
+for i in list1:
+    if i >= 0:
+        print (i)
+list2 = [12,4,-95,3]
+for num in list2:
+    if num >= 0:
+        print (" [ ",num," ] ")
+
+
+         
