076f46f9e51fde87d3d45dd023e8482e2ad2962e
sreeja002 <82311358+sreeja002@users.noreply.github.com>
2021-04-19 17:43:08 +0530

    create charfreq.py
    this will show you how many characters in a word

---
+++ charfreq.py
@@ -0,0 +1,13 @@
+string = input(" please enter a string: ")
+def char_frequency(string):
+    dict = { }
+    for i in string:
+        keys = dict.keys()
+        if n in keys:
+            dict[n]  += 1
+        else:
+            dict [n] = 1
+    return dict
+         
+            
+        
