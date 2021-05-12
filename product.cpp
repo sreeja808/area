7742caa2e7f7f51f1b841ab762261ff417616d7f
sreeja002 <82311358+sreeja002@users.noreply.github.com>
2021-05-12 11:17:05 +0530

    Create product.cpp
    This will give the product of two numbers given by user.

---
+++ product.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+	double num1,num2,productoftwonumbers;
+	cout <<"Enter two numbers:";
+	cin >>num1>>num2;
+	productoftwonumbers = num1*num2;
+	cout <<"Product:" <<productoftwonumbers;
+		
+}
